a92a09d54de9b7672348ace5a27d5ab176e30a27
Körtesi Gáspár <gazs@bergengocia.net>
2010-07-26 21:41:16 +0200

    üres sorokat töröljük

--- mr2.sh
+++ mr2.sh
@@ -1,7 +1,4 @@
 PERFORMER=$(ruby mr2akusztik.rb --list | zenity --list --column "Előadó")
-
-
 SAVEURL=$(zenity --file-selection --save --filename "$PERFORMER.mp3" --title "Hova töltsem le az MR2 Akusztik felvételt")
-
 ruby mr2akusztik.rb -p "$PERFORMER" -o "$SAVEURL" -d | zenity --progress --auto-kill --auto-close --title "MR2 Akusztik-$PERFORMER" --text "letöltés..."
 
