bcc0a7de3e18661f8a24081d424daf5077270521
Körtesi Gáspár <gazs@bergengocia.net>
2010-07-26 21:14:52 +0200

    Az este tanulsága: ha egy shellscript változót PATH-nak nevezel, utána ne csodálkozz, ha hirtelen egy exe se elérhető

--- mr2.sh
+++ mr2.sh
@@ -1,2 +1,7 @@
 PERFORMER=$(ruby mr2akusztik.rb --list | zenity --list --column "Előadó")
 
+
+SAVEURL=$(zenity --file-selection --save --filename "$PERFORMER.mp3" --title "Hova töltsem le az MR2 Akusztik felvételt")
+
+ruby mr2akusztik.rb -p "$PERFORMER" -o "$SAVEURL" -d | zenity --progress --auto-kill --auto-close --title "MR2 Akusztik-$PERFORMER" --text "letöltés..."
+
