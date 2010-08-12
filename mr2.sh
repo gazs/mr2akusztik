8e25e6f67489a84428f1469bcf93c9fa8a4f0b7b
Körtesi Gáspár <gazs@bergengocia.net>
2010-08-12 20:00:41 +0200

    más könyvtárból indítva is megy

--- mr2.sh
+++ mr2.sh
@@ -1,4 +1,4 @@
-PERFORMER=$(ruby mr2akusztik.rb --list | zenity --list --column "Előadó")
+PERFORMER=$(ruby $(dirname $0)/mr2akusztik.rb --list | zenity --list --column "Előadó")
 SAVEURL=$(zenity --file-selection --save --filename "$PERFORMER.mp3" --title "Hova töltsem le az MR2 Akusztik felvételt")
-ruby mr2akusztik.rb -p "$PERFORMER" -o "$SAVEURL" -d | zenity --progress --auto-kill --auto-close --title "MR2 Akusztik-$PERFORMER" --text "letöltés..."
+ruby $(dirname $0)/mr2akusztik.rb -p "$PERFORMER" -o "$SAVEURL" -d | zenity --progress --auto-kill --auto-close --title "MR2 Akusztik-$PERFORMER" --text "letöltés..."
 
